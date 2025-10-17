ce0826d5851e142f13f60c0e2be3e546dd83cd1a
keewie <sophiamore200@gmail.com>
2025-10-17 13:29:39 -0400

    a scene

---
+++ 2dGameProject/raccoon.gd
@@ -0,0 +1,20 @@
+extends Node2D
+@export var speed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		print("i pressed a button!")
+		position.x += speed	
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed
