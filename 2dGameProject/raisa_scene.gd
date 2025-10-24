6e46ae45d254a3413f81a7012d925ddd6afb3afb
rnoor1 <rnoor1@fordham.edu>
2025-10-24 01:23:53 -0400

    lab 3
    Raisa's scene

---
+++ 2dGameProject/raisa_scene.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@export var speed = 100
+
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	elif Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
